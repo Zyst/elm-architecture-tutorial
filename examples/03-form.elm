35fd08ae17d6d2090860b6c907128de24455fb6e
Erick Romero <erick.romero@aeris.net>
2018-09-27 23:21:19 -0500

    Hold my local stuff

--- examples/03-form.elm
+++ examples/03-form.elm
@@ -1,7 +1,9 @@
+module Main exposing (Model, Msg(..), init, main, update, validate, view, viewInput, viewValidation)
+
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
+import Html.Events exposing (onClick, onInput)
 
 
 
@@ -9,7 +11,7 @@ import Html.Events exposing (onInput)
 
 
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+    Browser.sandbox { init = init, update = update, view = view }
 
 
 
@@ -17,15 +19,17 @@ main =
 
 
 type alias Model =
-  { name : String
-  , password : String
-  , passwordAgain : String
-  }
+    { name : String
+    , age : Int
+    , password : String
+    , passwordAgain : String
+    , submitted : Bool
+    }
 
 
 init : Model
 init =
-  Model "" "" ""
+    Model "" 0 "" "" False
 
 
 
@@ -33,22 +37,30 @@ init =
 
 
 type Msg
-  = Name String
-  | Password String
-  | PasswordAgain String
+    = Name String
+    | Age String
+    | Password String
+    | PasswordAgain String
+    | Submit
 
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Name name ->
-      { model | name = name }
+    case msg of
+        Name name ->
+            { model | name = name, submitted = False }
 
-    Password password ->
-      { model | password = password }
+        Password password ->
+            { model | password = password, submitted = False }
 
-    PasswordAgain password ->
-      { model | passwordAgain = password }
+        PasswordAgain password ->
+            { model | passwordAgain = password, submitted = False }
+
+        Age age ->
+            { model | age = Maybe.withDefault 0 (String.toInt age), submitted = False }
+
+        Submit ->
+            { model | submitted = True }
 
 
 
@@ -57,22 +69,46 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ viewInput "text" "Name" model.name Name
-    , viewInput "password" "Password" model.password Password
-    , viewInput "password" "Re-enter Password" model.passwordAgain PasswordAgain
-    , viewValidation model
-    ]
+    div []
+        [ viewInput "text" "Name" model.name Name
+        , viewInput "number" "Age" (String.fromInt model.age) Age
+        , viewInput "password" "Password" model.password Password
+        , viewInput "password" "Re-enter Password" model.passwordAgain PasswordAgain
+        , button [ onClick Submit ] [ text "Submit" ]
+        , viewValidation model
+        ]
 
 
 viewInput : String -> String -> String -> (String -> msg) -> Html msg
 viewInput t p v toMsg =
-  input [ type_ t, placeholder p, value v, onInput toMsg ] []
+    input [ type_ t, placeholder p, value v, onInput toMsg ] []
+
+
+validate : Model -> Bool
+validate { password, passwordAgain, age } =
+    -- Passwords match
+    password
+        == passwordAgain
+        -- Password length is > 8
+        && String.length password
+        > 8
+        -- Password contains lowercase letters
+        && String.toUpper password
+        /= password
+        -- Password contains uppercase letters
+        && String.toLower password
+        /= password
+        -- Password contains a number
+        && String.any Char.isDigit password
 
 
 viewValidation : Model -> Html msg
 viewValidation model =
-  if model.password == model.passwordAgain then
-    div [ style "color" "green" ] [ text "OK" ]
-  else
-    div [ style "color" "red" ] [ text "Passwords do not match!" ]
+    if not model.submitted then
+        div [] []
+
+    else if validate model then
+        div [ style "color" "green" ] [ text "OK" ]
+
+    else
+        div [ style "color" "red" ] [ text "Passwords must match, be longer than 8 characters, must contain upper case, lower case, AND numeric characters. Age must be a number." ]
