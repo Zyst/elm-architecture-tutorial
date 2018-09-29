029127d35faec9df1ba68de36f162212541b6887
Erick Romero <erick.romero@aeris.net>
2018-09-28 21:02:50 -0500

    Prog

--- examples/04-random.elm
+++ examples/04-random.elm
@@ -1,7 +1,11 @@
+module Main exposing (Model, Msg(..), init, main, subscriptions, update, view)
+
 import Browser
 import Html exposing (..)
 import Html.Events exposing (..)
 import Random
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
 
 
 
@@ -9,12 +13,12 @@ import Random
 
 
 main =
-  Browser.element
-    { init = init
-    , update = update
-    , subscriptions = subscriptions
-    , view = view
-    }
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
 
 
 
@@ -22,15 +26,15 @@ main =
 
 
 type alias Model =
-  { dieFace : Int
-  }
+    { dieFace : Int
+    }
 
 
-init : () -> (Model, Cmd Msg)
+init : () -> ( Model, Cmd Msg )
 init _ =
-  ( Model 1
-  , Cmd.none
-  )
+    ( Model 1
+    , Cmd.none
+    )
 
 
 
@@ -38,22 +42,22 @@ init _ =
 
 
 type Msg
-  = Roll
-  | NewFace Int
+    = Roll
+    | NewFace Int
 
 
-update : Msg -> Model -> (Model, Cmd Msg)
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    Roll ->
-      ( model
-      , Random.generate NewFace (Random.int 1 6)
-      )
+    case msg of
+        Roll ->
+            ( model
+            , Random.generate NewFace (Random.int 1 6)
+            )
 
-    NewFace newFace ->
-      ( Model newFace
-      , Cmd.none
-      )
+        NewFace newFace ->
+            ( Model newFace
+            , Cmd.none
+            )
 
 
 
@@ -62,16 +66,52 @@ update msg model =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Sub.none
+    Sub.none
 
 
 
 -- VIEW
 
 
+createCircles : Int -> Svg msg
+createCircles face =
+    case face of
+        1 ->
+            circle
+                [ cx "75"
+                , cy "75"
+                , r "10"
+                , fill "black"
+                ]
+                []
+
+        _ ->
+            circle
+                [ r "0"
+                ]
+                []
+
+
 view : Model -> Html Msg
 view model =
-  div []
-    [ h1 [] [ text (String.fromInt model.dieFace) ]
-    , button [ onClick Roll ] [ text "Roll" ]
-    ]
+    div []
+        [ svg
+            [ width "150"
+            , height "150"
+            , viewBox "0 0 150 150"
+            ]
+            [ rect
+                [ x "10"
+                , y "10"
+                , width "130"
+                , height "130"
+                , rx "20"
+                , ry "20"
+                , fill "white"
+                , stroke "black"
+                ]
+                []
+            , createCircles model.dieFace
+            ]
+        , button [ onClick Roll ] [ Html.text "Roll" ]
+        ]
